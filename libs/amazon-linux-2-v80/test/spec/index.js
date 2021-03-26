f5bfd1e98a20bdf3a7c1efc026697362fa2be94f
Milan Felix Šulc <rkfelix@gmail.com>
2021-03-26 16:38:39 +0100

    [v80] added grpc

--- libs/amazon-linux-2-v80/test/spec/index.js
+++ libs/amazon-linux-2-v80/test/spec/index.js
@@ -5,9 +5,9 @@ const index = require('./../../dist/index');
 const toUnixPermissions = mode => '0' + (mode & parseInt('777', 8)).toString(8);
 
 test('count files', async () => {
-  expect(Object.keys(await index.getFiles()).length).toEqual(100);
+  expect(Object.keys(await index.getFiles()).length).toEqual(101);
   expect(Object.keys(await index.getPhpFiles()).length).toEqual(6);
-  expect(Object.keys(await index.getPhpModulesFiles()).length).toEqual(45);
+  expect(Object.keys(await index.getPhpModulesFiles()).length).toEqual(46);
   expect(Object.keys(await index.getSharedLibsFiles()).length).toEqual(49);
 });
 
